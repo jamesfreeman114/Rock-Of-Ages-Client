5d6e4cffe874f1d8a7f236b88492ee1f93df68ca
James Freeman <jamesfreeman114@gmail.com>
2026-03-24 20:12:33 -0500

    Expanded Rock List and changed intial state for rockState

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,14 +1,20 @@
+/* eslint-disable react/prop-types */
 import { useEffect } from "react"
 
 export const RockList = ({ rocks, fetchRocks }) => {
     useEffect(() => {
-    }, [])
+        fetchRocks()
+    }, [fetchRocks])
 
     const displayRocks = () => {
         if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-            </div>)
+            return rocks.map(rock => 
+                <div key={`key-${rock.id}`} 
+                className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
+                <div>{rock.name} ({rock.type.label}) weights {rock.weight}</div>
+                <div>In the collection of {rock.user.first_name} {rock.user.last_name}</div>  
+            </div>
+            )
         }
 
         return <h3>Loading Rocks...</h3>
