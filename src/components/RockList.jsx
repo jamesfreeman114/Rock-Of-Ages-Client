a88c3936bcdd5ad4c6d4bf3501a0bb4332f44734
James Freeman <jamesfreeman114@gmail.com>
2026-04-07 18:41:38 -0500

    Added User Rocks filter with delete button

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 import { useEffect } from "react"
 
-export const RockList = ({ rocks, fetchRocks }) => {
+export const RockList = ({ rocks, fetchRocks, showAll }) => {
     useEffect(() => {
-        fetchRocks()
-    }, [])
+        fetchRocks(showAll)
+    }, [showAll])
 
     const displayRocks = () => {
         if (rocks && rocks.length) {
@@ -12,7 +12,29 @@ export const RockList = ({ rocks, fetchRocks }) => {
                 <div key={`key-${rock.id}`} 
                 className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
                 <div>{rock.name} ({rock.type.label}) weights {rock.weight}</div>
-                <div>In the collection of {rock.user.first_name} {rock.user.last_name}</div>  
+                <div>In the collection of {rock.user.first_name} {rock.user.last_name}</div>
+                {
+                    showAll
+                        ? ""
+                        : <div>
+                    <button
+                        onClick={async () => {
+                            const response = await fetch(`http://localhost:8000/rocks/${rock.id}`,
+                                {
+                                    method: "DELETE",
+                                    headers: {
+                                        Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
+                                    }
+                                })
+                                if (response.status === 204) {fetchRocks(showAll)
+                            }
+                        }}
+                        className="border border-solid bg-red-700 text-white p-1">Delete
+                    </button>
+                </div>  
+                   
+                }
+                
             </div>
             )
         }
